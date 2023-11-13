0fbf95bb92c4a02e8ced5e93a53eb3ec418f96af
Niiranen Milo <niiranenmilo@gmail.com>
2023-11-13 08:55:47 +0200

    Branch testaus

---
+++ 01_BasicExcercises/FileOperations3/FileOperationsTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+using System.Reflection;
+
+namespace FileOperationsTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+            [TestMethod]
+            public void ReadFile_ReturnsListOfSettings_IfFileIsNotEmpty()
+            {
+                //Arrange
+              //  List<string> systemConfig;
+               // string winDir = "C:\\Windows";
+           // string path = "\\system.ini";
+            //Act
+           // systemConfig = Files.ReadFile(systemConfig, windir, path);
+            //Assert
+                // Assert.IsTrue(systemConfig.Count < 0);
+
+            }
+        
+
+    }
+}
